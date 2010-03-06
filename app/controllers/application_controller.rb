c504400cd9baa0d2fd6d834fcf09cc1a79f1efd3
Simon W. Henriksen <simonwh@gmail.com>
2010-03-06 16:21:21 +0100

    Force www before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
   include Twitter::AuthenticationHelpers
+  before_filter :force_www
   
   protect_from_forgery  
   helper :all
+  
+  DOMAINWITHWWW = "www.logthisworkout.com"
   
   def is_owner
     if User.find_by_id(params[:id]) != current_user
       flash[:alert] = "This profile is not available."
       redirect_to root_path
+    end
+  end
+  
+  def force_www
+    if request.env['HTTP_HOST'] != DOMAINWITHWWW && RAILS_ENV == 'production'
+      redirect_to DOMAINWITHWWW
     end
   end
 end

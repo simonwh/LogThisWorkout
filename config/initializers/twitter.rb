d47497219a91936230a04e64c12cfcd7fba4a05c
Simon W. Henriksen <simonwh@gmail.com>
2010-03-06 16:06:27 +0100

    Skiftet twitter_return_url til domæne

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -8,6 +8,6 @@ ACCESS_SECRET = 'QfbpROl4OmRLdN36GptvgFFFuuyOlKoP6W0VmDDcg'
 if RAILS_ENV == 'development'
   TWITTER_RETURN_URL = 'http://localhost:3000/finalize_login'
 else
-  TWITTER_RETURN_URL = 'http://logthisworkout.heroku.com/finalize_login'
+  TWITTER_RETURN_URL = 'http://www.logthisworkout.com/finalize_login'
 end
 #oauth = oauth.authorize_from_access('117796053-zyQ3TRjIWfjNIAqCUP5djRIgsaLQf9CM0zDH3FQd', 'xXtrhaBf5yZC5kjDNrwD7ZGyCwDn4zm5n89lJgSJ4')

64667de29d6098a6219c6e99c88f03895bf8bb40
Simon W. Henriksen <simonwh@gmail.com>
2010-03-08 14:10:41 +0100

    Typo...

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,6 +1,6 @@
 $('document').ready(function(){
 
-	$("#month_calender li").click(function(){
+	$("#month_calendar li").click(function(){
 		date = $(this).find('span').attr('class');
 		current = window.location.pathname
 		window.location.href = current + "/" + date
